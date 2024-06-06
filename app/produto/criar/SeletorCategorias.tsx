8ec90bbb3849d716f665cc56938c1c00b9c85a5f
Gustavo Feijó <gustavofeijo.dev@gmail.com>
2024-06-06 18:13:29 -0300

    Pagina para criação de novos produtos. Componente para seleção de categorias

---
+++ app/produto/criar/SeletorCategorias.tsx
@@ -0,0 +1,259 @@
+"use client";
+import {
+  ColumnFiltersState,
+  SortingState,
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Categorias } from "@prisma/client";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { FaArrowRightArrowLeft } from "react-icons/fa6";
+
+// Interface para lidar com a seleção das categorias.
+interface SelectCategorias extends Categorias {
+  select: boolean;
+}
+// ColumnHelper para a definição do funcionamento da tabela.
+const columnHelper = createColumnHelper<SelectCategorias[]>();
+const columns = [
+  columnHelper.accessor("select", {
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Selecionar categoria."
+      />
+    ),
+    enableSorting: false,
+  }),
+  columnHelper.accessor("nome_categoria", {
+    header: ({ column }) => (
+      <Button
+        type="button"
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Categoria
+        <FaArrowRightArrowLeft className="ml-2 h-4 w-4 rotate-90" />
+      </Button>
+    ),
+    cell: ({ row }) => <div>{row.getValue("nome_categoria")}</div>,
+  }),
+  columnHelper.accessor("id", {}),
+];
+
+// Componente contendo uma tabela de categorias, sendo possível realizar a seleção das mesmas.
+function SeletorCategoria({
+  categoriaSelecionada,
+}: {
+  categoriaSelecionada: (
+    categorias: {
+      id: string;
+      nome_categoria: string;
+    }[]
+  ) => void;
+}) {
+  // useStates para o funcionamento da tabela.
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [rowSelection, setRowSelection] = useState({});
+
+  // useState contendo os dados recebidos da API e useState para loading.
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // Instancia da tabela.
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onRowSelectionChange: setRowSelection,
+    initialState: {
+      pagination: { pageIndex: 0, pageSize: 4 },
+      columnVisibility: { id: false },
+    },
+    state: {
+      sorting,
+      columnFilters,
+      rowSelection,
+    },
+  });
+
+  //UseEffect para fetching da lista de categorias.
+  useEffect(() => {
+    const getCategorias = async () => {
+      try {
+        // Fetching da lista de categorias.
+        const response = await fetch("/api/categorias", { method: "GET" });
+        if (!response.ok) {
+          throw new Error(
+            `Não foi possível concluir a requisição. Status: ${response.status}`
+          );
+        }
+        const jsonData = await response.json();
+        setData(jsonData.result);
+      } catch (e: any) {
+        //No caso de erro, cria um toast para recarregar a pagina.
+        toast(e.message, {
+          description: "Um erro ocorreu. Tente recarregar a pagina",
+          action: {
+            label: "Recarregar",
+            onClick: () => {
+              const router = useRouter();
+              router.refresh();
+            },
+          },
+        });
+      } finally {
+        // Finaliza o loading.
+        setLoading(false);
+      }
+    };
+    getCategorias();
+  }, []);
+
+  // useEffect para fazer o update da seleção de categorias.
+  // Necessário visto que a implementação no onChange das checkbox apresenta certo delay na atualização dos estados.
+  useEffect(() => {
+    // Utiliza o modelo de seleção para iterar através de cada Row selecionada.
+    // Cria objetos contendo id e nome da categoria selecionada a variavel.
+    // Altera os dados do formulário através da função passada como prop.
+    const categoriasSel: { id: string; nome_categoria: string }[] = table
+      .getSelectedRowModel()
+      .flatRows.map((row) => ({
+        id: row.getValue("id") ?? "",
+        nome_categoria: row.getValue("nome_categoria") ?? "",
+      }));
+    categoriaSelecionada(categoriasSel);
+  }, [table.getSelectedRowModel()]);
+
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-around">
+      <div className="flex items-center py-4 w-2/3">
+        <Input
+          placeholder="Filtrar categorias..."
+          value={
+            (table.getColumn("nome_categoria")?.getFilterValue() as string) ??
+            ""
+          }
+          onChange={(event) =>
+            table
+              .getColumn("nome_categoria")
+              ?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+      </div>
+      <div className="rounded-md border h-[410px] w-2/3">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  {loading ? "LOADING..." : "No Results"}
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} de{" "}
+          {table.getFilteredRowModel().rows.length} categoria(s) selecionadas.
+        </div>
+        <div className="space-x-2">
+          <Button
+            type="button"
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Anterior
+          </Button>
+          <Button
+            type="button"
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Próxima
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default SeletorCategoria;
---
+++ app/produto/criar/page.tsx
@@ -0,0 +1,234 @@
+"use client";
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import { Button, buttonVariants } from "@/components/ui/button";
+import SeletorCategoria from "./SeletorCategorias";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { FaTrash } from "react-icons/fa6";
+import { cn } from "@/lib/utils";
+//Schema com Zod para validação do forms.
+const formSchema = z.object({
+  // String referente ao nome do produto cadastrado.
+  nome_produto: z
+    .string()
+    .min(3, { message: "O produto deve ter ao menos 3 caracteres." }),
+  // Number com o valor do estoque.
+  estoque: z.number({ message: "Deve ser inserido um numero." }).refine(
+    (num) => {
+      return num % 1 == 0;
+    },
+    { message: "Deve ser inserido um numero inteiro." }
+  ),
+  preco: z.number({ message: "Deve ser inserido um numero" }),
+  // Array de objetos, cada objeto sendo referente à uma categoria.
+  // Id é utilizado para a inserção dos dados, nome_categoria para display.
+  categorias: z
+    .array(
+      z.object({
+        id: z.string(),
+        nome_categoria: z.string(),
+      })
+    )
+    .min(1, { message: "Ao menos uma categoria deve ser selecionada." }),
+});
+
+// Função responsável pelo envio dos dados para o servidor.
+const onSubmit = async (formData: z.infer<typeof formSchema>) => {
+  console.log(formData);
+  const result = await fetch("/api/produtos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+  if (result.status == 200) {
+    const jsonRes = await result.json();
+    toast(jsonRes, {
+      description: new Date().toLocaleDateString("pt-BR", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }),
+      action: { label: "Okay", onClick: () => {} },
+    });
+  } else {
+    const jsonErr = await result.json();
+    toast(jsonErr, {
+      description: "Um erro ocorreu.",
+      action: { label: "Okay", onClick: () => {} },
+    });
+  }
+};
+
+// Component referente ao formulário de envio.
+function page() {
+  // Hook para criação do formulário baseado no schema criado.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      nome_produto: "",
+      estoque: 0,
+      preco: 0,
+    },
+  });
+
+  // Função para alterar a lista de categorias armazenada nos dados do formulário.
+  // Passada como props para a tabela de seleção.
+  const categoriaSelecionada = (
+    categorias: {
+      id: string;
+      nome_categoria: string;
+    }[]
+  ) => {
+    form.setValue("categorias", categorias);
+  };
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex  w-[800px] h-[500px] p-12 items-center justify-around border rounded-lg shadow shadow-slate-600 max-[600px]:flex-col max-[600px]:h-fit"
+      >
+        <div className="flex flex-col items-center">
+          <FormField
+            control={form.control}
+            name="nome_produto"
+            render={({ field }) => (
+              <FormItem className="w-4/5">
+                <FormLabel>Produto</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Insira o nome do produto"
+                    className="flex-1"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>Nome do novo produto.</FormDescription>{" "}
+                <div className="h-8">
+                  <FormMessage />
+                </div>
+              </FormItem>
+            )}
+          />
+          <div className="flex items-center justify-around">
+            <FormField
+              control={form.control}
+              name="preco"
+              render={({ field }) => (
+                <FormItem className="w-2/5">
+                  <FormLabel>Preço</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Insira o preço..."
+                      className="w-full"
+                      type="number"
+                      step="0.01"
+                      min={0}
+                      {...field}
+                      onChange={(e) => {
+                        if (e.target.value.length > 0)
+                          field.onChange(e.target.valueAsNumber);
+                      }}
+                    />
+                  </FormControl>
+                  <FormDescription>Preço do produto.</FormDescription>{" "}
+                  <div className="h-8">
+                    <FormMessage />
+                  </div>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="estoque"
+              render={({ field }) => (
+                <FormItem className="w-2/5">
+                  <FormLabel>Estoque</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Insira o estoque..."
+                      className="w-full"
+                      type="number"
+                      min={0}
+                      {...field}
+                      onChange={(e) => {
+                        if (e.target.value.length > 0)
+                          field.onChange(e.target.valueAsNumber);
+                      }}
+                    />
+                  </FormControl>
+                  <FormDescription>Estoque do produto.</FormDescription>{" "}
+                  <div className="h-8">
+                    <FormMessage />
+                  </div>
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="mb-4">
+            <DropdownMenu>
+              <DropdownMenuTrigger
+                className={buttonVariants({ variant: "ghost" })}
+              >
+                Categorias
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="p-4 max-h-96 overflow-scroll ">
+                <DropdownMenuLabel>Selecionadas</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {form.getValues("categorias") &&
+                  form.getValues("categorias").map((categoria, index) => (
+                    <DropdownMenuItem
+                      className="p-4 justify-between"
+                      key={index}
+                    >
+                      {categoria.nome_categoria}
+                    </DropdownMenuItem>
+                  ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <Button type="submit" variant="default">
+            Cadastrar
+          </Button>
+        </div>
+        <FormField
+          control={form.control}
+          name="categorias"
+          render={() => (
+            <FormItem className="h-full">
+              <FormControl>
+                <SeletorCategoria categoriaSelecionada={categoriaSelecionada} />
+              </FormControl>
+              <div className="h-8">
+                <FormMessage className="text-center" />
+              </div>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
+
+export default page;
